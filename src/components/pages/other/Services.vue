a395cbdb68dc6335dc7307fc91679c4a97242713
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-12-18 16:17:01 +0200

    continue clearing...

--- src/components/pages/other/Services.vue
+++ src/components/pages/other/Services.vue
@@ -4,9 +4,6 @@
       <v-col cols="12" md="6" lg="4">
         <v-card flat class="transparent">
           <v-toolbar flat>
-            <!-- <v-toolbar-title>
-              Services
-            </v-toolbar-title> -->
             <v-spacer />
             <v-btn icon @click="newService">
               <v-icon>$add</v-icon>
