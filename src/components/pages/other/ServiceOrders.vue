f51d07e79d40bbaa13232b06faec7e7477df2f67
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2021-01-09 21:48:49 +0200

    Service Orders modified

--- src/components/pages/other/ServiceOrders.vue
+++ src/components/pages/other/ServiceOrders.vue
@@ -1,35 +1,19 @@
 <template>
-  <v-container class="mb-12">
-    <v-navigation-drawer absolute
-      floating
-      class="homefone"
-      v-model="drawer"
-      :mini-variant.sync="mini"
-      width="480"
-    >
-        <v-list-item class="px-2">
-          <v-btn icon large>
-            <v-icon>$pdf</v-icon>
-          </v-btn>
-
-          <v-list-item-title class="ml-4">
-            <v-row>
-              <v-col cols="6">
-                <small>Company</small>
-              </v-col>
-              <v-col cols="6">
-                <small>Product/Service Type</small>
-              </v-col>
-            </v-row>
-
+  <v-container fluid fill-height>
+    <v-navigation-drawer
+        dark
+        app
+        v-model="drawer"
+        :mini-variant.sync="mini"
+        :mini-variant-width="60"
+        permanent
+      >
+        <v-list-item class="px-2 my-2">
+          <v-list-item-title>
+            <v-btn icon @click="mini = !mini">
+              <v-icon>{{ mini ? 'mdi-chevron-right' : 'mdi-chevron-left' }}</v-icon>
+            </v-btn>
           </v-list-item-title>
-
-          <v-btn
-            icon
-            @click.stop="mini = !mini"
-          >
-            <v-icon>mdi-menu-left-outline</v-icon>
-          </v-btn>
         </v-list-item>
 
         <v-divider></v-divider>
@@ -43,24 +27,21 @@
             <v-list-item-icon></v-list-item-icon>
 
           <v-list-item-content @click="click(item)">
-            <v-row>
-              <v-col cols="6">
-                <v-list-item-title class="text-left">{{ item.companyName }}</v-list-item-title>
-              </v-col>
-              <v-col cols="6">
-                <v-list-item-title class="text-left">{{ item.product }}</v-list-item-title>
-              </v-col>
-            </v-row>
+            <v-list-item-title class="text-left">
+              {{ item.companyName }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <ServiceDetails
-      v-if="currentOrderId"
-      :data.sync="currentOrder"
-      class="mt-12"
-    />
+    <v-row justify="center">
+      <ServiceDetails
+        v-if="currentOrderId"
+        :data.sync="currentOrder"
+        class="mt-12"
+      />
+    </v-row>
   </v-container>
 </template>
 
